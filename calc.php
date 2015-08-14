49118852519040c40f97dc88618ec54a06037f62
core01 <core01@bk.ru>
2015-08-14 19:03:10 +0600

    added html code

--- calc.php
+++ calc.php
@@ -5,4 +5,15 @@
  * Date: 14.08.2015
  * Time: 15:48
  */
-echo "Hello world!";
+?>
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+Привет, Аасо, как дела?
+</body>
+</html>
+
