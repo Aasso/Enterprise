fe3fffa84cfcedff8e0553bc5b0eba0ac12b0a37
Aasso <ziro.aasso@gmail.com>
2015-08-15 15:09:48 +0400

    Простая форма

--- calc.php
+++ calc.php
@@ -10,10 +10,25 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Калькулятор</title>
+    <style>
+  body{
+        position:absolute;
+      	top: 100px;
+      	left: 200px;
+  }
+    </style>
 </head>
 <body>
-
+<form action="" method="POST">
+    <label for="one">Первое число</label>
+    <input type="text" id="one" name="first">
+    <label for="operator">Введите оператор</label>
+    <input type="text" id="operator" name="oper">
+    <label for="two">Второе число</label>
+    <input type="text" id="two">
+    <input type="submit" value="Вычислить">
+</form>
 </body>
 </html>
 
